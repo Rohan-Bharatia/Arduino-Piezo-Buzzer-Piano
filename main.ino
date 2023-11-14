af2c7ad3128ab572e01c0e7dbd8d8b309a4331c1
Rohan Bharatia <144117280+Rohan-Bharatia@users.noreply.github.com>
2023-11-14 13:12:48 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,24 +2,20 @@
 
 const int pitchPin[8] = {3, 4, 5, 6, 7, 8, 9, 10};
 const int piezoBuzzerPin = 13;
-const int pitch[8] = {
-  analogRead(pitchPin[0]),
-  analogRead(pitchPin[1]),
-  analogRead(pitchPin[2]),
-  analogRead(pitchPin[3]),
-  analogRead(pitchPin[4]),
-  analogRead(pitchPin[5]),
-  analogRead(pitchPin[6]),
-  analogRead(pitchPin[7])
-};
+const int pitch[8];
 
 void setup()
-{
+{ 
   for(int i = 0; i < 8; i++)
   {
     pinMode(pitchPin[i], INPUT);
   }
   pinMode(piezoBuzzerPin, INPUT);
+
+  for(int i = 0; i < 8; i++)
+  {
+    pitch[i] = analogRead(pitchPin[i]);
+  }
 }
 
 void playPitches(int min, int max)
